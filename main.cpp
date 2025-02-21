107932752aff7f9ad96e4ccf3ce6335f1a46afe8
Sala K 405 <salak405@salasfr.itm.edu.co>
2025-02-21 17:36:18 -0500

    La main del proyecto

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
+#include "./Tecnico.h"
 
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout<<"Hello World";
+int main() {
 
-    return 0;
+	Carro goku;
+	Tecnico vegeta;
+
+	Carro* ptr = &goku;  // ptr, nombre cualquiera -- Puntero
+
+
+	vegeta.Tanquear(ptr,20);
+	
+	goku.Informacion();
+	
+	//	cout << "Sobrante " <<  sobrante2 << endl; 
+	AutoServicio maquina;
+	
+	maquina.ListarPrecios();
+	//maquina.Recargar(1000);
+    vegeta.Recargar(&maquina, 300000);
+    vegeta.Recargar(&maquina, 200000);
+    vegeta.Recargar(&maquina, 200000);
+    
+    goku.Informacion();
+    maquina.Tanquear(&goku,0);
+    goku.Recorrer(100);
+    goku.Informacion();
+    cout << maquina.saldo;
+
+
+    //cout << sizeof(carro); //Tamaño de goku en bytes
+    
+
+	return 0;
 }
+
+
